7b32f83c038355508981e982f3b069fd7d3e4987
SourLemon <lemon4sour@gmail.com>
2023-04-20 00:46:05 +0300

    rebuilt base

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using artikbaslioyz.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace artikbaslioyz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private denemeEntities2 db = new denemeEntities2();
+
+        // GET: Home
+      public ActionResult HomePage()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HomePage([Bind(Include = "username,uname,surname,email,pword")] deneme4 element)
+        {
+            if (ModelState.IsValid)
+            {
+                db.deneme4.Add(element);
+                db.SaveChanges();
+                return RedirectToAction("HomePage");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="artikbaslioyz.MvcApplication" Language="C#" %>
